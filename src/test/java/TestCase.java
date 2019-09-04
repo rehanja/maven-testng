5c9fd4693a65a66314f2b35a6edb1d0320cfaf1d
Your Name <rehanjananga2015@gmail.com>
2019-09-04 11:02:10 +0530

    first commit

---
+++ src/test/java/TestCase.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase {
+
+    public static void main (String [] args){
+
+        System.out.println("Hello");
+    }
+ /*   WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void test() {
+        driver.get("https://github.com/rehanja/");
+       Assert.assertEquals(driver.getTitle(), "Rehanja");
+        driver.quit();
+    }*/
+}
